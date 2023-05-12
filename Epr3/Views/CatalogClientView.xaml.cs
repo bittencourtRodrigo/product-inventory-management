50396ab9586a87531b222875db9915f94565b16c
Rodrigo Bittencourt <rodrigo.adsti@gmail.com>
2023-05-12 13:47:59 -0300

    Implemented binding and dependency injection

--- Epr3/Views/CatalogClientView.xaml.cs
+++ Epr3/Views/CatalogClientView.xaml.cs
@@ -1,9 +1,12 @@
+using Epr3.ViewModels;
+
 namespace Epr3.Views;
 
 public partial class CatalogClientView : ContentPage
 {
-	public CatalogClientView()
+	public CatalogClientView(CatalogClientViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
