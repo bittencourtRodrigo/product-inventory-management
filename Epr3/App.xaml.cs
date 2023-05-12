0456163295cab5746a2eb5a40ea09814c538f07e
Rodrigo Bittencourt <rodrigo.adsti@gmail.com>
2023-05-12 08:43:15 -0300

    Add project files.

---
+++ Epr3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Epr3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Epr3/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Epr3;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
