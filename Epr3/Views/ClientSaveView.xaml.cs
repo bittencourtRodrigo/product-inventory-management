c27ce0edf1b3fad32d489ad3866a0d6ac86e613b
Rodrigo Bittencourt <rodrigo.adsti@gmail.com>
2023-05-12 12:02:45 -0300

    Implemented binding in ClientSaveView

--- Epr3/Views/ClientSaveView.xaml.cs
+++ Epr3/Views/ClientSaveView.xaml.cs
@@ -1,9 +1,12 @@
+using Epr3.ViewModels;
+
 namespace Epr3.Views;
 
 public partial class ClientSaveView : ContentPage
 {
-	public ClientSaveView()
+	public ClientSaveView(ClientSaveViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
